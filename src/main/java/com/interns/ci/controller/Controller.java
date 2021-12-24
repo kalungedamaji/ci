b9bea6c9a4451c1745c9ad170ff81e8c94b4befa
damaji <damaji.kalunge@technogise.com>
2021-12-24 17:47:43 +0530

    Spring boot Application

---
+++ src/main/java/com/interns/ci/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.interns.ci.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("controller/hello")
+    public String sayHellow(){
+        return "Hello from Rest Controller";
+    }
+}
