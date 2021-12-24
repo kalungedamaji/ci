b30f4c32ff581c2935fe60dbbf563e0fd9dd9509
damaji <damaji.kalunge@technogise.com>
2021-12-24 20:37:31 +0530

    CI WorkFlow

--- src/test/java/com/interns/ci/CiApplicationTests.java
+++ src/test/java/com/interns/ci/CiApplicationTests.java
@@ -1,10 +1,15 @@
 package com.interns.ci;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class CiApplicationTests {
 
-
-
+ @Test
+    public void test(){
+     assertTrue(false);
+ }
 }
