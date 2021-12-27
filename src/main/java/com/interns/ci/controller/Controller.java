da3987524be0a885b9f2f08fadc9a5563714f7d9
damaji <damaji.kalunge@technogise.com>
2021-12-27 12:00:04 +0530

    Deploy Code to Heroku

--- src/main/java/com/interns/ci/controller/Controller.java
+++ src/main/java/com/interns/ci/controller/Controller.java
@@ -8,6 +8,6 @@ public class Controller {
 
     @GetMapping("controller/hello")
     public String sayHello(){
-        return "Hello from Rest Controller from Heroku CI Flow ";
+        return "Hello from Rest Controller deployed from Github Action to Heroku ";
     }
 }
