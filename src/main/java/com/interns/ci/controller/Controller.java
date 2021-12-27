bb64d0cac7a458bc0f5995ba4af06460dcb7ae6f
Damaji <kalungedamaji1137@gmail.com>
2021-12-27 15:12:17 +0530

    Feture 1 (#5)
    ENV Creation Change
    
    Co-authored-by: damaji <damaji.kalunge@technogise.com>

--- src/main/java/com/interns/ci/controller/Controller.java
+++ src/main/java/com/interns/ci/controller/Controller.java
@@ -9,6 +9,7 @@ public class Controller {
     @GetMapping("controller/hello")
     public String sayHello(){
         return "Hello from Rest Controller deployed from Github Action to Heroku " +
-                "Feature branch merged into master using pull request";
+                "\n Feature branch merged into master using pull request" +
+                "\n dev to stage automatic deployment";
     }
 }
