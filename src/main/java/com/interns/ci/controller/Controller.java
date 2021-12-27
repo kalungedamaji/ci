bf037cdf81c7aef5c3203fb89906927ee53ba8a7
Damaji <kalungedamaji1137@gmail.com>
2021-12-27 12:19:18 +0530

    Commit in feature branch. (#1)
    Co-authored-by: damaji <damaji.kalunge@technogise.com>

--- src/main/java/com/interns/ci/controller/Controller.java
+++ src/main/java/com/interns/ci/controller/Controller.java
@@ -8,6 +8,7 @@ public class Controller {
 
     @GetMapping("controller/hello")
     public String sayHello(){
-        return "Hello from Rest Controller deployed from Github Action to Heroku ";
+        return "Hello from Rest Controller deployed from Github Action to Heroku " +
+                "Feature branch merged into master using pull request";
     }
 }
