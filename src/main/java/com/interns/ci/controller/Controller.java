de0543ac86a4f8dac91b9a87d1f0df72ece1d8ac
damaji <kalungedamaji1137@gmail.com>
2021-12-28 13:18:05 +0530

    Changed ENV Prop

--- src/main/java/com/interns/ci/controller/Controller.java
+++ src/main/java/com/interns/ci/controller/Controller.java
@@ -11,6 +11,7 @@ public class Controller {
         return "Hello from Rest Controller deployed from Github Action to Heroku " +
                 "</br> Feature branch merged into master using pull request" +
                 "</br> Feature to DEV ENV Automatic Deployment." +
-                "</br> Dev to Stage ENV after Manual Approval ";
+                "</br> Dev to Stage ENV Automatic Deployment after 3 min" +
+                "</br> Dev to Stage ENV Automatic Deployment after 2 min";
     }
 }
