120842cf4b68bfe175cc024628dcc2bc45b8bf2d
dkalunge <kalungedamaji1137@gmail.com>
2022-06-08 10:24:23 +0530

    some change

--- src/main/java/com/interns/ci/controller/Controller.java
+++ src/main/java/com/interns/ci/controller/Controller.java
@@ -8,8 +8,9 @@ public class Controller {
 
     @GetMapping("controller/hello")
     public String sayHello(){
-        return "Hello from Rest Controller deployed from Github Action to Heroku " +
+        return "Hello from Rest Controller deployed from Github Action to Heroku 1 " +
                 "</br> Feature branch merged into master using pull request" +
+                "</br> Build docker image." +
                 "</br> Feature to DEV ENV Automatic Deployment." +
                 "</br> Dev to Stage ENV Automatic Deployment after 3 min" +
                 "</br> Dev to Stage ENV Automatic Deployment after 2 min";
