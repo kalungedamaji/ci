65ec8b8970eb012515d1966ea36c66b7cc800ec6
dkalunge <kalungedamaji1137@gmail.com>
2022-06-08 10:28:52 +0530

    removed some change

--- src/main/java/com/interns/ci/controller/Controller.java
+++ src/main/java/com/interns/ci/controller/Controller.java
@@ -8,7 +8,7 @@ public class Controller {
 
     @GetMapping("controller/hello")
     public String sayHello(){
-        return "Hello from Rest Controller deployed from Github Action to Heroku 1 " +
+        return "Hello from Rest Controller deployed from Github Action to Heroku " +
                 "</br> Feature branch merged into master using pull request" +
                 "</br> Build docker image." +
                 "</br> Feature to DEV ENV Automatic Deployment." +
